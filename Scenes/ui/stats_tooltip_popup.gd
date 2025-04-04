a850bc558b152be2e99ff21a657bb9f561b424d5
Alex <alexblake544@gmail.com>
2025-04-04 16:53:30 -0400

    Tooltip Stuff -- Altering code

--- Scenes/ui/stats_tooltip_popup.gd
+++ Scenes/ui/stats_tooltip_popup.gd
@@ -1,32 +1,29 @@
 class_name StatsTooltipPopup
 extends Control
 
-const PARTY_PANEL_SCENE := preload("res://Scenes/ui/party_panel.tscn")
-
-@onready var tooltip_stats : CenterContainer = %TooltipStats
 @onready var stat_description : RichTextLabel = %StatsDescription
 
 
 func _ready():
-	for panel : PartyPanel in tooltip_stats.get_children():
-		panel.queue_free()
+	hide_tooltip()
+	await get_tree().create_timer(1).timeout
+	show_tooltip(preload("res://Resources/Characters/ishil.tres"))
 
 
 func show_tooltip(stats : CharacterStats) -> void:
-	var new_panel := PARTY_PANEL_SCENE.instantiate() as PartyPanel
-	tooltip_stats.add_child(new_panel)
-	new_panel.stats = stats
-	new_panel.tooltip_requested.connect(hide_tooltip.unbind(1))
-	stat_description.text = " STR: " + str(stats.strength) + "\n MAG: " + str(stats.magic) + "\n SPD: " + str(stats.speed)
+	stat_description = RichTextLabel.new()
+	stat_description.text = " " + str(stats.strength) + "\n " + str(stats.magic) + "\n SPD: " + str(stats.speed)
+	add_child(stat_description)
 	show()
 
 
 func hide_tooltip() -> void:
 	if not visible:
 		return
-	
-	for stats : PartyPanel in tooltip_stats.get_children():
-		stats.queue_free()
+
+	if stat_description:
+		stat_description.queue_free()
+		stat_description = null
 
 	hide()
 
